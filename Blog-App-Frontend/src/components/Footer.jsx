f977cf9287bf415282495ad86defd6e3e3aec891
GundaSaiDeepthi <23eg105r52@anurag.edu.in>
2026-05-18 12:05:16 +0530

    Updated

--- Blog-App-Frontend/src/components/Footer.jsx
+++ Blog-App-Frontend/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./Footer.css";
 
@@ -5,11 +6,17 @@ function Footer() {
   return (
     <footer className="footer">
       
+      {/* Simple Footer Content */}
+      <div className="footer-content">
+        <h3>BlogApp</h3>
+        <p>Share ideas, explore articles.</p>
+      </div>
 
       {/* Bottom */}
       <div className="footer-bottom">
-        <p>© 2026 BlogSphere. All Rights Reserved.</p>
+        <p>© 2026 BlogApp. All Rights Reserved.</p>
       </div>
+
     </footer>
   );
 }
